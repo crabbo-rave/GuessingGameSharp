5fe6cdd0598efa119e378c540c797cdba827a14e
static-binary <vatisprocurator@gmail.com>
2022-01-04 12:39:12 -0500

    More idiomatic

--- GuessingGameSharp/Program.fs
+++ GuessingGameSharp/Program.fs
@@ -34,7 +34,7 @@ let parseRange args =
         | None -> failwith "Range was not provided"
         | Some x -> x
 
-let runGame range : (GameState * GameState) option =
+let runGame range : GameState =
     let random = Random()
     let number = random.Next(1, range+1)
     Console.WriteLine $"OK, let's start. I am thinking of a number between 1 to {range}"
@@ -52,43 +52,50 @@ let runGame range : (GameState * GameState) option =
                 match userInput with
                 | Parse input ->
                     guess <- input
-                    if gameState.Guesses.Contains guess then
-                        Console.WriteLine "You already guessed that!!"
-                        Some (gameState, gameState)
-                    elif 0 >= guess || guess > range then
-                        Console.WriteLine $"Sorry, {guess} is less than or equal to zero or greater than {range}."
-                        gameState <- { gameState with Count = gameState.Count - 1 }
-                        Some (gameState, gameState)
-                    elif guess < gameState.Target then
-                        Console.WriteLine $"{guess} is too low!"
-                        Some (gameState, gameState)
-                    elif guess > gameState.Target then
-                        Console.WriteLine $"{guess} is too high!"
-                        Some (gameState, gameState)
-                    else    
-                        gameState <- { gameState with Termination = Some Won }
-                        None
-                    gameState.Guesses.Add(guess)
-                    gameState <- { gameState with Count = gameState.Count + 1 }
+                    let next =
+                        if gameState.Guesses.Contains guess then
+                            Console.WriteLine "You already guessed that!!"
+                            Some (gameState, gameState)
+                        elif 0 >= guess || guess > range then
+                            Console.WriteLine $"Sorry, {guess} is less than or equal to zero or greater than {range}."
+                            gameState <- { gameState with Count = gameState.Count - 1 }
+                            Some (gameState, gameState)
+                        elif guess < gameState.Target then
+                            Console.WriteLine $"{guess} is too low!"
+                            Some (gameState, gameState)
+                        elif guess > gameState.Target then
+                            Console.WriteLine $"{guess} is too high!"
+                            Some (gameState, gameState)
+                        else
+                            gameState <- { gameState with Termination = Some Won }
+                            None
+                    gameState <- { gameState with Count = gameState.Count + 1;Guesses = gameState.Guesses.Add(guess) }
                     Console.WriteLine "Enter your guess: "
+                    next
                 | _ as input ->
                     if input.Equals "give up" then
                         gameState <- { gameState with Termination = Some GaveUp }
-                        None 
+                        None
                     else
                         Console.WriteLine $"\"{userInput}\" should be an integer: "
                         Some (gameState, gameState)
                 )
             |> Seq.last
 
-let displayEndState (hasWon, number, count) =
-    match hasWon with
-    | true ->
+let displayEndState (state: GameState) =
+    match state.Termination with
+    | Some termination ->
+        match termination with
+        | Won ->
+            Console.Clear()
+            Console.WriteLine $"Yay! you guessed my number!! It took you {state.Count} tries! (Not counting bad inputs.)"
+        | _ ->
+            Console.Clear()
+            Console.WriteLine $":( You gave up. The number was {state.Target}. It took you {state.Count} tries. (Not counting bad inputs.)"
+    | _ -> 
         Console.Clear()
-        Console.WriteLine $"Yay! you guessed my number!! It took you {count} tries! (Not counting bad inputs.)"
-    | _ ->
-        Console.Clear()
-        Console.WriteLine $":( You gave up. The number was {number}. It took you {count} tries. (Not counting bad inputs.)"
+        failwith "Error processing termination"
+        
 
 [<EntryPoint>]
 let main args =
