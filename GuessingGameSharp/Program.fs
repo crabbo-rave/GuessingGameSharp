8f92fad909f33cd272e504a79399272b355da139
static-binary <vatisprocurator@gmail.com>
2022-01-03 19:10:17 -0500

    More idiomatic with Seq.unfold

--- GuessingGameSharp/Program.fs
+++ GuessingGameSharp/Program.fs
@@ -28,49 +28,59 @@ let clamp (lower,upper) x =
   else x
 
 let parseRange args =
-  Array.tryHead args
-  |> Option.map (parse>>clamp (1,350))
-  |> function
-    | None -> failwith "Range was not provided"
-    | Some x -> x
+    Array.tryHead args
+    |> Option.map (parse>>clamp (1,350))
+    |> function
+        | None -> failwith "Range was not provided"
+        | Some x -> x
 
-let runGame range =
+let runGame range : (GameState * GameState) option =
     let random = Random()
-    let mutable count = 0
-    let guesses = List<int>()
-    let mutable hasWon = false
     let number = random.Next(1, range+1)
     Console.WriteLine $"OK, let's start. I am thinking of a number between 1 to {range}"
     Console.WriteLine "Enter your guess: "
     let mutable guess = 0
-    let mutable Break = false
-    while not Break do
-        let userInput = Console.ReadLine()
-        match userInput with
-        | Parse input ->
-            guess <- input
-            if guesses.Contains guess then
-                Console.WriteLine "You already guessed that!!"
-            elif 0 >= guess || guess > range then
-                Console.WriteLine $"Sorry, {guess} is less than or equal to zero or greater than {range}."
-                count <- count - 1
-            elif guess < number then
-                Console.WriteLine $"{guess} is too low!"
-            elif guess > number then
-                Console.WriteLine $"{guess} is too high!"
-            else
-                hasWon <- true
-                Break <- true
-            guesses.Add(guess)
-            count <- count + 1
-            Console.WriteLine "Enter your guess: "
-        | _ as input ->
-            if input.Equals "give up" then
-                hasWon <- false
-                Break <- true
-            else
-                Console.WriteLine $"\"{userInput}\" should be an integer: "
-    hasWon, number, count
+    let mutable gameState = 
+        {
+            Guesses = Set.empty
+            Target = number
+            Count = 0
+            Termination = None
+        }
+    gameState |> Seq.unfold (fun game -> 
+                let userInput = Console.ReadLine()
+                match userInput with
+                | Parse input ->
+                    guess <- input
+                    if gameState.Guesses.Contains guess then
+                        Console.WriteLine "You already guessed that!!"
+                        Some (gameState, gameState)
+                    elif 0 >= guess || guess > range then
+                        Console.WriteLine $"Sorry, {guess} is less than or equal to zero or greater than {range}."
+                        gameState <- { gameState with Count = gameState.Count - 1 }
+                        Some (gameState, gameState)
+                    elif guess < gameState.Target then
+                        Console.WriteLine $"{guess} is too low!"
+                        Some (gameState, gameState)
+                    elif guess > gameState.Target then
+                        Console.WriteLine $"{guess} is too high!"
+                        Some (gameState, gameState)
+                    else
+                        gameState <- { gameState with Termination = Some Won }
+                        None
+                    gameState.Guesses.Add(guess)
+                    gameState <- { gameState with Count = gameState.Count + 1 }
+                    Console.WriteLine "Enter your guess: "
+                    Some (gameState, gameState)
+                | _ as input ->
+                    if input.Equals "give up" then
+                        gameState <- { gameState with Termination = Some GaveUp }
+                        None
+                    else
+                        Console.WriteLine $"\"{userInput}\" should be an integer: "
+                        Some (gameState, gameState)
+                Some (gameState, gameState) )
+            |> Seq.last
 
 let displayEndState (hasWon, number, count) =
     match hasWon with
